fa66d17ef854d2189a8f0d9d4d206c9239b4b573
Alejandro Cabezas Garríguez <alejandro.cabezas@alumnos.upm.es>
2022-06-02 19:20:47 +0200

    Initial commit

---
+++ src/main/java/com/upm/miw/tfm/eatitproductsapp/EatItProductsAppApplication.java
@@ -0,0 +1,13 @@
+package com.upm.miw.tfm.eatitproductsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatItProductsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatItProductsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upm/miw/tfm/eatitproductsapp/EatItProductsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upm.miw.tfm.eatitproductsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatItProductsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
