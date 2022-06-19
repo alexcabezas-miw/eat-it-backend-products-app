44c4366f78d56ae127e39a98fbe9a711937d3640
Alex Cabezas <alexcabezas0705@gmail.com>
2022-06-19 12:14:38 +0200

    Fix nullpointer when mapping product without ingredients

--- src/main/java/com/upm/miw/tfm/eatitproductsapp/service/mapper/ProductsMapper.java
+++ src/main/java/com/upm/miw/tfm/eatitproductsapp/service/mapper/ProductsMapper.java
@@ -7,6 +7,7 @@ import com.upm.miw.tfm.eatitproductsapp.service.model.Product;
 import com.upm.miw.tfm.eatitproductsapp.web.dto.product.ProductListDTO;
 import org.mapstruct.Mapper;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +22,9 @@ public interface ProductsMapper {
     }
 
     default List<String> fromIngredientList(List<Ingredient> source) {
+        if(source == null) {
+            return Collections.emptyList();
+        }
         return source.stream().map(Ingredient::getName).collect(Collectors.toList());
     }
 }
