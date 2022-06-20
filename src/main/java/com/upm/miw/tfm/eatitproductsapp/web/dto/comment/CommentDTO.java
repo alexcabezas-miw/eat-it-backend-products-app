3cfabf49c2b94308f2b62b10de3f810b565453ac
Alex Cabezas <alexcabezas0705@gmail.com>
2022-06-20 21:05:15 +0200

    Changed data type

--- src/main/java/com/upm/miw/tfm/eatitproductsapp/web/dto/comment/CommentDTO.java
+++ src/main/java/com/upm/miw/tfm/eatitproductsapp/web/dto/comment/CommentDTO.java
@@ -3,13 +3,13 @@ package com.upm.miw.tfm.eatitproductsapp.web.dto.comment;
 import lombok.Builder;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @Builder
 public class CommentDTO {
     private String username;
-    private LocalDateTime createdDate;
+    private LocalDate createdDate;
     private String content;
     private int rate;
 }
